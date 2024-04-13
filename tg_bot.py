e9786e6e8265509bff284f76a8ddbad5c9c451cd
Makar_Popkov <zadi.super.99@mail.ru>
2024-04-13 15:37:12 +0300

    клавиатура + отменение изменения имени

--- tg_bot.py
+++ tg_bot.py
@@ -1,11 +1,11 @@
 import logging
 from telegram.ext import Application, MessageHandler, filters, CommandHandler, ApplicationBuilder
 from telegram.ext import ConversationHandler
+from telegram import ReplyKeyboardMarkup
 import sqlite3
 
 BOT_TOKEN = '6556960280:AAHF9q6-1qUlesWwx-M-rxL946oiGl2ceFE'
 
-# Запускаем логгирование
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
 )
@@ -13,14 +13,27 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-# Определяем функцию-обработчик сообщений.
-# У неё два параметра, updater, принявший сообщение и контекст - дополнительная информация о сообщении.
+def main():
+    application = Application.builder().token(BOT_TOKEN).build()
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('change_name', change_name)],
+        states={
+            1: [MessageHandler(filters.TEXT & ~filters.COMMAND, change_name_response)]
+        },
+        fallbacks=[MessageHandler(filters.TEXT, change_name_response)])
+
+    application.add_handler(CommandHandler('start', start))
+    application.add_handler(conv_handler)
+
+
+    application.run_polling()
+
+
 async def start(update, context):
-    # У объекта класса Updater есть поле message,
-    # являющееся объектом сообщения.
-    # У message есть поле text, содержащее текст полученного сообщения,
-    # а также метод reply_text(str),
-    # отсылающий ответ пользователю, от которого получено сообщение.
+    reply_keyboard = [['/change_name']]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+
     username = update.message.chat.username
     con = sqlite3.connect('db/blogs.db')
     cur = con.cursor()
@@ -32,45 +45,34 @@ async def start(update, context):
         text = "Вы еще не зарегистрированы. \n" \
                "Пожалуйста зарегистрируйтесь: http://127.0.0.1:8080/register"
     con.close()
-    await update.message.reply_text(text)
+    await update.message.reply_text(text, reply_markup=markup)
 
 
 async def change_name(update, context):
-    await update.message.reply_text('Пожалуйста введите новоё имя персонажа:')
+    reply_keyboard = [['Отмена']]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+    await update.message.reply_text('Пожалуйста введите новоё имя персонажа:', reply_markup=markup)
     return 1
 
 
 async def change_name_response(update, context):
+    reply_keyboard = [['/change_name']]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+
     new_name = update.message.text
-    username = update.message.chat.username
-    con = sqlite3.connect('db/blogs.db')
-    cur = con.cursor()
-    cur.execute(f"""UPDATE users SET name = '{new_name}' WHERE login = '{username}'""")
-    con.commit()
-    con.close()
-    await update.message.reply_text("Имя успешно изменено!")
+    if new_name != 'Отмена':
+        username = update.message.chat.username
+        con = sqlite3.connect('db/blogs.db')
+        cur = con.cursor()
+        cur.execute(f"""UPDATE users SET name = '{new_name}' WHERE login = '{username}'""")
+        con.commit()
+        con.close()
+        await update.message.reply_text("Имя успешно изменено!", reply_markup=markup)
+    else:
+        await update.message.reply_text("Изменения отменены.", reply_markup=markup)
     return ConversationHandler.END
 
 
-def main():
-    application = Application.builder().token(BOT_TOKEN).build()
-    application.add_handler(CommandHandler('start', start))
-
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('change_name', change_name)],
-        states={
-            1: [MessageHandler(filters.TEXT & ~filters.COMMAND, change_name_response)]
-        },
-        fallbacks=[MessageHandler(filters.TEXT, change_name_response)]
-    )
-
-    application.add_handler(conv_handler)
-
-    # Запускаем приложение.
-    application.run_polling()
-
-
-# Запускаем функцию main() в случае запуска скрипта.
 if __name__ == '__main__':
     main()
 
