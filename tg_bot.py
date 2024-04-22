aaf0c8b9ef27526502090a2fab5a10bf54589afb
Makar_Popkov <zadi.super.99@mail.ru>
2024-04-22 16:54:32 +0300

    отображение инвентаря в тг боте

--- tg_bot.py
+++ tg_bot.py
@@ -26,12 +26,14 @@ def main():
     application.add_handler(CommandHandler('start', start))
     application.add_handler(conv_handler)
 
+    application.add_handler(CommandHandler('inventory', inventory))
+
 
     application.run_polling()
 
 
 async def start(update, context):
-    reply_keyboard = [['/change_name']]
+    reply_keyboard = [['/change_name'], ['/inventory']]
     markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 
     username = update.message.chat.username
@@ -55,8 +57,35 @@ async def change_name(update, context):
     return 1
 
 
+async def inventory(update, context):
+    reply_keyboard = [['/change_name'], ['/inventory']]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+
+    username = update.message.chat.username
+    con = sqlite3.connect('db/blogs.db')
+    cur = con.cursor()
+    result = list(cur.execute(f"""SELECT hands, body, legs, head FROM users WHERE login is 
+                             '{username}'"""))[0]
+    print(result)
+    hands = list(cur.execute(f"""SELECT name, about FROM items WHERE id is 
+                             '{result[0]}'"""))[0]
+    body = list(cur.execute(f"""SELECT name, about FROM items WHERE id is 
+                                 '{result[1]}'"""))[0]
+    legs = list(cur.execute(f"""SELECT name, about FROM items WHERE id is 
+                                 '{result[2]}'"""))[0]
+    head = list(cur.execute(f"""SELECT name, about FROM items WHERE id is 
+                                 '{result[3]}'"""))[0]
+    con.close()
+    text = "На вас сейчас следующее снаряжение: \n" \
+            f"Голова - {head[0]}. {head[1]} \n" \
+            f"Тело - {body[0]}. {body[1]} \n" \
+            f"Руки - {hands[0]}. {hands[1]} \n" \
+            f"Ноги - {legs[0]}. {legs[1]}"
+    await update.message.reply_text(text, reply_markup=markup)
+
+
 async def change_name_response(update, context):
-    reply_keyboard = [['/change_name']]
+    reply_keyboard = [['/change_name'], ['/inventory']]
     markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 
     new_name = update.message.text
